0f91c41da9d3a5e866be34b730f309eb19a2ab6b
Itaty Cisneros <301710679@bis.ute.edu.mx>
2019-03-29 10:57:09 -0600

    Se creó la clase CourseViewModel y la clase Column

---
+++ Tutorias/Tutorias/ViewModels/Column.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tutorias.ViewModels
+{
+    public class Column
+    {
+        public string name { get; set; }
+
+        public string color { get; set; }
+
+        public Nullable<double> y { get; set; }
+    }
+}
---
+++ Tutorias/Tutorias/ViewModels/CourseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Tutorias.ViewModels
+{
+    public class CourseViewModel
+    {
+        [Required(ErrorMessage = "El campo Materia es obligatorio")]
+        [Display(Name = "Materia")]
+        public string Materia { get; set; }
+
+        [Required(ErrorMessage = "El campo Maestro es obligatorio")]
+        [Display(Name = "Nombre del maestro")]
+        public string Maestro { get; set; }
+    }
+}
